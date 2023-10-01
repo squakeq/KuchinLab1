b05cb091a43d0afe6c1e125d6acde96ca30efd88
squakeq <kuchimon@mail.ru>
2023-10-01 15:54:20 +0300

    added ChangePipeStatus function and corresponding menu action

--- KuchinLab1/KuchinLab1.cpp
+++ KuchinLab1/KuchinLab1.cpp
@@ -71,6 +71,29 @@ void ShowMenu()
 		 << "7. Load" << "\n"
 		 << "8. Exit" << "\n"
 }
+void ChangePipeStatus(vector<Pipe>& pipes, string pipe_name)
+{
+	bool name_exists = false;
+	int ind = -1;
+	for (Pipe pipe : pipes) {
+		ind++;
+		if (pipe.name = pipe_name) {
+			name_exists = true;
+			if (pipe.is_repairing) {
+				pipe.is_repairing = false;
+				break;
+
+			}
+			pipe.is_repairing = true;
+			pipes[ind] = pipe;
+
+		}
+	}
+	if (!name_exists) {
+		cout << "Wrong name! Try again. \n";
+	}
+	cout << "Status Changed!\n";
+}
 istream operator >> (istream& in, compress_Station& new_cs)
 {
 	cout << "Type name: ";
@@ -106,40 +129,123 @@ ostream& operator << (ostream& out, const Compress_station& cs)
 }
 istream& operator >> (istream& in, Pipe& new_pipe)
 {
-	cout << "TY"
+	cout << "Type name: ";
+	cin >> new_pipe.name;
+	cout << "Type length: ";
+	cin >> new_pipe.length;
+	while (cin.fail() || new_pipe.length <= 0)
+	{
+		cin.clear();
+		cin.ignore(int(pow(10, 6)), '\n');
+		cout << "Type correct info (>0): ";
+		cin >> new_pipe.length;
+	}
+	cout << "Type diameter: ";
+	cin >> new_pipe.diameter;
+	while (cin.fail() || new_pipe.diameter <= 0)
+	{
+		cin.clear();
+		cin.ignore(int(pow(10, 6)), '\n');
+		cout << "Type correct info (>0): ";
+		cin >> new_pipe.diameter;
+	}
+	return in;
 }
+ostream& operator << (ostream& out, const Pipe& pipe)
 {
 	string repair_status = "False";
 	if (pipe.is_repairing)
 	{
 		repair_status = "True";
 	}
-	cout << ".........." << "\n"
+	out << ".........." << "\n"
 		<< "Pipe Name : " << pipe.name << "\n"
 		<< "Pipe lenght:" << pipe.length << "\n"
 		<< "Pipe deameter:" << pipe.length << "\n"
 		<< "Is repairing:" << pipe.repair_status << "\n";
+	return out;
+
 }
 int main()
 {
+	vector <Pipe> pipes;
+	vector <compress_Station> stations;
+
 	while (1)
 	{
 		ShowMenu();
 		int action = 0;
+		cout << "Type nimber(1-8): ";
 		cin >> action;
 		switch (action)
 		{
 		case 1:
 		{
-			CreatePipe();
+			Pipe new_pipe;
+			cin >> new_pipe;
+			bool name_exists = false;
+			for (Pipe pipe : pipes) {
+				if (pipe.name == new_pipe.name) {
+					cout << "Name already exists!\n";
+					name_exists = true;
+					break;
+				}
+			}
+			if (!name_exists) {
+				pipes.push_back(new_pipe);
+
+			}
 			break;
 		}
 		case 2:
 		{
-			CreateCS();
+			Compress_station new_cs;
+			cin >> new_cs;
+			bool name_exists = false;
+			for (Compress_station cs : stations) {
+				if (cs.name == new_cs.name) {
+					cout << "Name already exists!\n";
+					name_exists = true;
+					break;
+				}
+			}
+			if (!name_exists) {
+				stations.push_back(new_cs);
+			}
 			break;
 		}
-
+		case 3:
+		{
+			cout << "Pipes:\n";
+			for (Pipe pipe : pipes) {
+				cout << pipe;
+			}
+			cout << "----------\n"
+				<< "Compress Stations:\n";
+			for (Compress_station cs : stations) {
+				cout << cs;
+			}
+			cout << "\n";
+			break;
+		}
+		case 4:
+		{
+			string pipe_name = "";
+			cout << "Type pipe name: ";
+			cin >> pipe_name;
+			ChangePipeStatus(pipes, pipe_name);
+			break;
+		}
+		case 8:
+		{
+			return 0;
+		}
+		default:
+		{
+			cout << "Wrong action!\n";
+			cin.clear();
+			cin.ignore(int(pow(10, 6)), '\n');
+		}
 		}
 	}
 }
